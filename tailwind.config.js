4f91aa04b931cdce44f6dcc772b000520b3f279b
Ơ̄եsuեsu𐌊i θ Ꮖᴨꭰꭱa <madara303uc@gmail.com>
2026-05-02 19:07:16 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      animation: {
-        'pulse-slow': 'pulse 8s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        mono: ['var(--font-mono)'],
       },
     },
   },
