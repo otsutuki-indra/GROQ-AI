3f9bd5b8cb3d4182eb90d78d7876b63fee81e1e7
Ơ̄եsuեsu𐌊i θ Ꮖᴨꭰꭱa <madara303uc@gmail.com>
2026-05-02 16:20:27 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+'use client'
 
-import ChatWindow from "./components/ChatWindow"
-import CodePreview from "./components/CodePreview"
+import { ChatWindow } from './components/ChatWindow'
+import { CodePreview } from './components/CodePreview'
+import { useStore } from './store/useStore'
 
 export default function Home() {
+  const { code, language } = useStore()
+
   return (
-    <main className="flex h-screen gap-4 p-4">
-      <ChatWindow />
-      <CodePreview />
+    <main className="flex h-screen gap-4 p-6 bg-[#030712]">
+      <div className="flex-1"><ChatWindow /></div>
+      <div className="flex-1"><CodePreview code={code} language={language} /></div>
     </main>
   )
 }
