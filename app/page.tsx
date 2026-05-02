a76874572f6bccc586ac0714ac93d29eccdf48b3
Ơ̄եsuեsu𐌊i θ Ꮖᴨꭰꭱa <madara303uc@gmail.com>
2026-05-02 16:27:41 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
 'use client'
 
 import { ChatWindow } from './components/ChatWindow'
-import { CodePreview } from './components/CodePreview'
+import CodePreview from './components/CodePreview' // Fixed: Removed curly braces
 import { useStore } from './store/useStore'
 
 export default function Home() {
   const { code, language } = useStore()
 
   return (
-    <main className="flex h-screen gap-4 p-6 bg-[#030712]">
-      <div className="flex-1"><ChatWindow /></div>
-      <div className="flex-1"><CodePreview code={code} language={language} /></div>
+    <main className="flex h-screen w-full gap-4 p-6 bg-[#030712] overflow-hidden">
+      {/* Background Aurora Decor */}
+      <div className="fixed inset-0 pointer-events-none">
+        <div className="absolute top-[-10%] right-[-10%] w-[500px] h-[500px] bg-emerald-500/10 blur-[120px] rounded-full" />
+        <div className="absolute bottom-[-10%] left-[-10%] w-[500px] h-[500px] bg-blue-500/5 blur-[120px] rounded-full" />
+      </div>
+
+      {/* Main UI Layout */}
+      <div className="relative z-10 flex w-full h-full gap-4">
+        <div className="flex-1 min-w-0 h-full">
+          <ChatWindow />
+        </div>
+        <div className="flex-1 min-w-0 h-full">
+          <CodePreview code={code} language={language} />
+        </div>
+      </div>
     </main>
   )
 }
